8d96b9d5215ea09da42849e57e51496d5f5ae195
Eduardo Ruiz Rios <eduardo.ruiz.rios09@gmail.com>
2022-09-03 21:27:19 -0500

    mod: sobrescribe metodo onCreate para mostrar la MainActivity

--- CalculadoraTrigonometrica/app/src/main/java/com/example/calculadoratrigonometrica/MainActivity.java
+++ CalculadoraTrigonometrica/app/src/main/java/com/example/calculadoratrigonometrica/MainActivity.java
@@ -1,6 +1,15 @@
 package com.example.calculadoratrigonometrica;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); // load the activity_main.xml content
+    }
 }
