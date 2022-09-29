732420bcf5d2f88470c89d6feaef68fcaa943a91
Kevin Salgado <sgkevinsg@gmail.com>
2022-09-28 22:34:47 -0500

    correciones jdk

---
+++ Memegrafia/app/src/main/java/com/example/memegrafia/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.memegrafia;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+
+
+import com.example.memegrafia.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        var view = binding.getRoot();
+        setContentView(view);
+
+        setSupportActionBar(binding.toolbar);
+        if (getSupportActionBar () != null) {
+            getSupportActionBar ().setTitle ("Memegrafia");
+        }
+
+    }
+}
