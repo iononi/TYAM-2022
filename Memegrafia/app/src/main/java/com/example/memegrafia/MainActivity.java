791f195e5f2d0c51a1c525f016da77a27c2837ce
Eduardo Ruiz Rios <eduardo.ruiz.rios09@gmail.com>
2022-09-30 01:49:41 -0500

    add: contiene la logica del programa principal

--- Memegrafia/app/src/main/java/com/example/memegrafia/MainActivity.java
+++ Memegrafia/app/src/main/java/com/example/memegrafia/MainActivity.java
@@ -1,22 +1,17 @@
 package com.example.memegrafia;
-import android.nfc.Tag;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
+import android.widget.FrameLayout;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.splashscreen.SplashScreen;
-import androidx.room.Room;
-import androidx.room.RoomDatabase;
-import androidx.sqlite.db.SupportSQLiteDatabase;
-
+import androidx.fragment.app.FragmentTransaction;
 
 
 import com.example.memegrafia.databinding.ActivityMainBinding;
+
+import com.example.memegrafia.Fragments.Details;
+import com.example.memegrafia.Fragments.MemeList;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
@@ -29,10 +24,30 @@ public class MainActivity extends AppCompatActivity {
         var view = binding.getRoot();
         setContentView(view);
 
-        setSupportActionBar(binding.toolbar);
-        if (getSupportActionBar () != null) {
-            getSupportActionBar ().setTitle ("Memegrafia");
-        }
+        MemeList memeList = new MemeList();
+        memeList.setMemeSelectedListener(position -> {
+            FrameLayout layout = findViewById (R.id.contentDetails);
+            System.out.println(layout);
 
+            if (layout != null) {
+                //System.out.println("Entro al if... layout = " + layout);
+                getSupportFragmentManager ()
+                        .beginTransaction ()
+                        .replace (R.id.contentDetails, new Details (position))
+                        .setTransition (FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                        .commit ();
+            } else {
+                //System.out.println("Entro al intent...");
+                Intent intent = new Intent (this, DetailsActivity.class);
+                intent.putExtra ("POSITION", position);
+                startActivity (intent);
+            }
+        });
+
+        getSupportFragmentManager ()
+                .beginTransaction ()
+                .add (R.id.myContainer, memeList)
+                .setTransition (FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                .commit ();
     }
 }
