0009a5cbf60af5a7cb7da1f5b543ff6c648b71d7
Eduardo Ruiz Rios <eduardo.ruiz.rios09@gmail.com>
2022-10-01 15:33:46 -0500

    mod: añade la capacidad de mostrar imagenes
    Añade un array de recursos (```ìnt[] drawables```) drawables que
    contiene los recursos de las imagenes a mostrar.
    
    Tambien añade comentarios al codigo.

--- Memegrafia/app/src/main/java/com/example/memegrafia/Fragments/MemeList.java
+++ Memegrafia/app/src/main/java/com/example/memegrafia/Fragments/MemeList.java
@@ -1,17 +1,17 @@
 package com.example.memegrafia.Fragments;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.memegrafia.R;
@@ -27,6 +27,7 @@ public class MemeList extends Fragment {
     @Nullable
     @Override
     public View onCreateView (@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // infla el diseño del recycler view
         return inflater.inflate (R.layout.fragment_list, container, false);
     }
 
@@ -38,69 +39,27 @@ public class MemeList extends Fragment {
     public void onActivityCreated (@Nullable Bundle savedInstanceState) {
         super.onActivityCreated (savedInstanceState);
 
-        // el método getActivity regresa una referencia a la actividad que hospeda al framento
+        // el método getActivity regresa una referencia a la actividad que hospeda al fragmento
         FragmentActivity activity = getActivity ();
         if (activity == null) return;
 
         String [] namesArray = activity.getResources ().getStringArray (R.array.names);
+        int [] drawables = {R.drawable.cereal_guy, R.drawable.f_yeah, R.drawable.forever_alone,
+            R.drawable.freddie_mercury, R.drawable.lol_guy, R.drawable.neil_degrasse_tyson,
+            R.drawable.oh_crap, R.drawable.okay, R.drawable.rage_guy, R.drawable.troll_face,
+            R.drawable.y_u_no_guy, R.drawable.yao_ming};
         List<String> memes = Arrays.asList (namesArray);
 
+        // referencia al recycler view
         RecyclerView recyclerView = activity.findViewById (R.id.myList);
-        if (recyclerView ==  null) return;
+        if (recyclerView ==  null) return; // no existe recycler view
 
-        recyclerView.setLayoutManager (new LinearLayoutManager(activity, RecyclerView.VERTICAL, false));
+        recyclerView.setLayoutManager (new GridLayoutManager(activity, 2));
 
         // se crea un objeto MyAdapter al que se pasan los parámetros necesarios
         // context: la actividad que hospeda al fragmento
         // List<Memes>: la lista con la información al mostrar
         // OnMemeSelected: el objeto listener mediante el cuál se invocará al manejador del evento memeSelected para los elementos de la lista
-        recyclerView.setAdapter (new MyAdapter (activity, memes, this.listener));
-    }
-}
-
-
-class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
-    private Context context;
-    private List<String> memes;
-    private OnMemeSelected listener;
-
-    MyAdapter(Context context, List<String> memes, OnMemeSelected listener) {
-        this.context = context;
-        this.memes = memes;
-        this.listener = listener;
-    }
-
-    @NonNull
-    @Override
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        LayoutInflater inflater = LayoutInflater.from(context);
-        View view = inflater.inflate(android.R.layout.simple_list_item_1, parent, false);
-        return new MyViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, final int position) {
-        // se asigna la información en cada fila de la lista y se establece el manejador del evento OnClickListener,
-        // de modo que al seleccionarse se invoque al método planetSelected en el objeto que manejará dicho evento
-        holder.setData(memes.get(position));
-        holder.itemView.setOnClickListener(view -> listener.onMemeSelected(position));
-    }
-
-    @Override
-    public int getItemCount() {
-        return memes.size();
-    }
-
-    class MyViewHolder extends RecyclerView.ViewHolder {
-        private TextView text1;
-
-        MyViewHolder(@NonNull View itemView) {
-            super(itemView);
-            text1 = itemView.findViewById(android.R.id.text1);
-        }
-
-        void setData(String data) {
-            text1.setText(data);
-        }
+        recyclerView.setAdapter (new MyAdapter (memes, drawables, this.listener));
     }
 }
