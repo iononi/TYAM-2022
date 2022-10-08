b323b4dcb778c011a3b6b9a4402834bb0d035188
Kevin Salgado <sgkevinsg@gmail.com>
2022-10-08 09:43:46 -0500

    error no corregido en el gradle

---
+++ LoremPicsum/app/src/main/java/com/example/lorempicsum/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.lorempicsum;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.LinkedList;
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (getSupportActionBar()!=null){
+            getSupportActionBar().setTitle(Utils.TITLE);
+        }
+    }
+}
