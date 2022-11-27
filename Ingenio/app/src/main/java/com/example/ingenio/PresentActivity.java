8b2ef89354d66f966395230447dccef0a056190f
Kevin Salgado <sgkevinsg@gmail.com>
2022-11-26 19:08:49 -0600

    implementation del navigation menu

--- Ingenio/app/src/main/java/com/example/ingenio/PresentActivity.java
+++ Ingenio/app/src/main/java/com/example/ingenio/PresentActivity.java
@@ -4,28 +4,72 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toolbar;
+
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class PresentActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class PresentActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_present);
 
+        //Hooks
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        //Tool bar
+        setSupportActionBar(toolbar);
+
+        //Navigation Drawer Menu
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        //codigo que no sirve pero me da cosa borrarlo y que luego lo requiera
+        /*Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("Ingenio");
-        setActionBar(toolbar);
+        setActionBar(toolbar);*/
 
     }
 
     @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem){
+        return true;
+    }
+
+
+
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.main, menu);
         return super.onCreateOptionsMenu(menu);
@@ -42,5 +86,5 @@ public class PresentActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 }
