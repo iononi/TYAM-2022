942c645e8e1f9277f7399c9c7832276aee178978
Eduardo Ruiz Rios <eduardo.ruiz.rios09@gmail.com>
2022-11-30 00:25:29 -0600

    nueva actividad de registro, sin funcionalidad

--- Ingenio/app/src/main/java/com/example/ingenio/FormActivity.java
+++ Ingenio/app/src/main/java/com/example/ingenio/FormActivity.java
@@ -1,15 +1,20 @@
 package com.example.ingenio;
 
-import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class FormActivity extends Activity {
+import com.example.ingenio.databinding.ActivityFormBinding;
 
+public class FormActivity extends AppCompatActivity {
+
+    ActivityFormBinding binding;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_form);
+        binding = ActivityFormBinding.inflate (getLayoutInflater ());
+        var registerView = binding.getRoot ();
+        setContentView(registerView);
     }
 }
